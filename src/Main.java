2e008158f7fcf2ffecb9eec6c39fcf2e6630c99d
kokanoka <kokanoka30@gmail.com>
2021-08-06 18:00:48 +0300

    Проверка номера карты 36606829379587 fail

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "3537571898605254";
+        String number = "36606829379587";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
